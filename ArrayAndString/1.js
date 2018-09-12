94f127e8c9c470cafa65b40076e25213e01ca14d
fengyong <fkworld@foxmail.com>
2018-09-12 19:14:26 +0800

    add 1

---
+++ ArrayAndString/1.js
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode-cn.com/problems/two-sum/description/
+ * - 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+ * - 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+    return -1
+};
