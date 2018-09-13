1ce0c9b8612745c7e98c8d941393a1378c2337fd
fengyong <fkworld@foxmail.com>
2018-09-13 19:11:10 +0800

    add 9

---
+++ 9.js
@@ -0,0 +1,31 @@
+/**
+ * 9.回文数
+ * - https://leetcode-cn.com/problems/palindrome-number/description/
+ * - 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * - 你能不将整数转为字符串来解决这个问题吗？
+ */
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    // 负数一定不是回文数
+    if (x < 0) { return false }
+
+    let len = x.toString().length
+    // 子方法：获取整数x倒数第n位的值（从0开始）
+    const f = function (x, index) {
+        return Math.floor(x / (10 ** index)) % 10
+    }
+    for (let i = 0; i < len / 2; i++) {
+        if (f(x, i) != f(x, len - i - 1)) {
+            return false
+        }
+    }
+    return true
+};
+
+// test
+let r = isPalindrome(12222221)
+console.log(r)
