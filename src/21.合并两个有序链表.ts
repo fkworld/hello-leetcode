57018c043b27447066ff0de116b96a000443cffd
fengyong <fkworld@foxmail.com>
2020-09-21 18:08:14 +0800

    add: 21

---
+++ src/21.合并两个有序链表.ts
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=typescript
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+interface ListNode {
+  val: number;
+  next: ListNode | null;
+}
+
+function mergeTwoLists(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  // 迭代
+  const prehead = new ListNode(0);
+
+  let prev = prehead;
+  while (l1 != null && l2 != null) {
+    if (l1.val <= l2.val) {
+      prev.next = l1;
+      l1 = l1.next;
+    } else {
+      prev.next = l2;
+      l2 = l2.next;
+    }
+    prev = prev.next;
+  }
+
+  // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+  prev.next = l1 === null ? l2 : l1;
+
+  return prehead.next;
+}
+// @lc code=end
